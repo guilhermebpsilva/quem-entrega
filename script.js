f4c8a718ec156dd5e8b92c6b26f3cf32339f6470
guilhermebpsilva <gbpsilva0@gmail.com>
2022-11-27 10:42:18 -0300

    added tracking history

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const submitBtn = document.querySelector('.submit');
+const submitForm = document.querySelector('.form');
 const trackingCode = document.querySelector('#codeInsert')
+const historyList = document.querySelector('.list');
 
 let track = ""
 const regexCorreios = /^[A-Z]{2}[0-9]{9}BR$/i
 
-function showUp() {
-    track = trackingCode.value
+function showUp(e) {
+    e.preventDefault();
+    track = trackingCode.value;
+
     if(regexCorreios.test(track) === true) {
         console.log("É Correios!");
+        let newItem = document.createElement('li');
+        newItem.appendChild(document.createTextNode(track));
+        historyList.appendChild(newItem);
     } else {
         console.log("Não é Correios!")
     }
 }
 
-submitBtn.addEventListener("click", showUp);
+submitForm.addEventListener("submit", showUp);
